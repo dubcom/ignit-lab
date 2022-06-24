c8dae77c8516f919096a96afb0fe2229dc2534e9
Fernando Chucre <marcos@dubcom.com.br>
2022-06-24 11:55:04 -0300

    fix componet video and variable of env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx',
+    './src/**/*.tsx'
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.png)'
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
